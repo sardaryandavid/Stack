18fcd35397b7ac1c69d6d049ef38b1f20e5d2c3f
sardaryandavid <74458820+sardaryandavid@users.noreply.github.com>
2021-10-21 19:21:13 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
 #include <stdio.h>
 #include <iostream>
 
+#define TYPE char
+#include "stack.h"
+
+#define TYPE int
 #include "stack.h"
 
 int main()
 {
+    struct myStack_int stack3, stack4;
+    stackConstructor(&stack3);
+    stackConstructor(&stack4);
+
+    stackPush(&stack3, 4);
+    stackPush(&stack3, 6);
+    stackPush(&stack3, 2);
+    stackPush(&stack3, 1);
+
+    stackPop(&stack3);
+
+    stackDump(&stack3, noMistake, INFORMATION);
+
+    struct myStack_char stack1, stack2;
+    stackConstructor(&stack1);
+    stackConstructor(&stack2);
+
+    stackPush(&stack1, 'a');
+    stackPush(&stack1, 'b');
+    stackPush(&stack1, 'd');
+    stackPush(&stack1, 'f');
+
+    stackPop(&stack1);
+
+    stackDump(&stack1, noMistake, INFORMATION);
 
     return 0;
 }
--- stack.h
+++ stack.h
@@ -2,33 +2,172 @@
 #define STACK_H_INCLUDED
 
 #include <cstdlib>
+#include <stdio.h>
+#include <assert.h>
+#include <cstdlib>
+#include <cstring>
+
 
 #define INFORMATION __FILE__, __LINE__, __PRETTY_FUNCTION__
 #define printInformation printf("\x1b[32mFilename: %s, line of program: %d, function: %s\x1b[0m", INFORMATION);
 
-#define CAT(x,y) x#y
-#define myStack(x) CAT("myStack_", x) //
-#define TYPE char
+#define CAT(x,y) x##y
+#define CATforType(x,y) #x#y
+
+#define myStack(x) CAT(myStack_, x)
+#define myStackType(x) CATforType(myStack_, x)
+
+#define getName(name) #name
 
 enum keyOfMistake {nullStackPtr = -100, nullPtrOnStack, negativeSize, noMistake};
 
-struct myStack {
+struct myStack(TYPE) {
   TYPE* ptrOnStack = nullptr;
   size_t sizeOfStack = -1;
 
   const size_t maxSizeOfStack = 100;
 };
 
-void stackConstructor(struct myStack* Stack);
-void stackDestructor(struct MyStack* Stack);
+void stackConstructor(struct myStack(TYPE)* Stack);
+void stackDestructor(struct myStack(TYPE)* Stack);
 
-void stackAssert(struct myStack* Stack, const char* fileOfMistake, int stringOfMistake, const char* functionOfMistake);
-int stackIsGood(const struct myStack* Stack);
-void stackDump(struct myStack* Stack, int keyOfMistake, const char* fileOfMistake, int stringOfMistake, const char* functionOfMistake);
+void stackAssert(struct myStack(TYPE)* Stack, const char* fileOfMistake, int stringOfMistake, const char* functionOfMistake);
+int stackIsGood(const struct myStack(TYPE)* Stack);
 
-void stackPush(struct myStack* Stack, const TYPE value);
-void stackPop(struct myStack* Stack);
-void stackResize(struct myStack* Stack, size_t newSize);
-void printStack(struct myStack* Stack);
+void stackDump(struct myStack(TYPE)* Stack,     int keyOfMistake,
+               const char* fileOfMistake, int stringOfMistake,
+               const char* functionOfMistake);
+
+
+void stackPush(struct myStack(TYPE)* Stack, const TYPE value);
+void stackPop(struct myStack(TYPE)* Stack);
+void stackResize(struct myStack(TYPE)* Stack, size_t newSize);
+void printStack(struct myStack(TYPE)* Stack);
+
+void stackConstructor(struct myStack(TYPE)* Stack) {
+    Stack->ptrOnStack = (TYPE*) calloc(Stack->maxSizeOfStack, sizeof(Stack->maxSizeOfStack));
+    Stack->sizeOfStack = 0;
+
+    stackAssert(Stack, INFORMATION);
+}
+
+void stackDestructor(struct myStack(TYPE)* Stack) {
+    stackAssert(Stack, INFORMATION);
+
+    free(Stack->ptrOnStack);
+}
+
+void stackPush(struct myStack(TYPE)* Stack, const TYPE value) {
+    stackAssert(Stack, INFORMATION);
+
+    if(Stack->sizeOfStack + 1 > Stack->maxSizeOfStack) {
+        Stack->ptrOnStack = (TYPE*) realloc(Stack->ptrOnStack, Stack->sizeOfStack * 2);
+    }
+
+    *(Stack->ptrOnStack + Stack->sizeOfStack++) = value;
+
+    stackAssert(Stack, INFORMATION);
+}
+
+void stackPop(struct myStack(TYPE)* Stack) {
+    stackAssert(Stack, INFORMATION);
+
+    if(Stack->sizeOfStack <= 0) {
+        stackDump(Stack, negativeSize, INFORMATION);
+    }
+
+    *(Stack->ptrOnStack + Stack->sizeOfStack--) = 0;
+
+    stackAssert(Stack, INFORMATION);
+}
+
+void stackAssert(struct myStack(TYPE)* Stack, const char* fileOfMistake, int stringOfMistake, const char* functionOfMistake) {
+    int keyOfMistake = 0;
+
+    if((keyOfMistake = stackIsGood(Stack)) == nullStackPtr   ||
+       (keyOfMistake = stackIsGood(Stack)) == nullPtrOnStack ||
+       (keyOfMistake = stackIsGood(Stack)) == negativeSize) {
+        stackDump(Stack, keyOfMistake, fileOfMistake, stringOfMistake, functionOfMistake);
+    }
+}
+
+int stackIsGood(const struct myStack(TYPE)* Stack) {
+    if(!Stack) {
+        return nullStackPtr;
+    }
+
+    if(!Stack->ptrOnStack){
+        return nullPtrOnStack;
+    }
+
+    if(Stack->sizeOfStack < 0) {
+        return negativeSize;
+    }
+
+    return noMistake;
+}
+
+void stackDump(struct myStack(TYPE)* Stack, int keyOfMistake,
+               const char* fileOfMistake, int stringOfMistake, const char* functionOfMistake) {
+    printf("\x1b[32m  Filename: %s, line of program: %d, function: %s  \x1b[0m\n",
+            fileOfMistake, stringOfMistake, functionOfMistake);
+    printf("Stack type: %s\n", myStackType(TYPE));
+
+    switch (keyOfMistake) {
+        case nullStackPtr:
+            printf("key of mistake: null pointer on struct MyStack\n");
+            break;
+        case nullPtrOnStack:
+            printf("key of mistake: null pointer on stack\n");
+            break;
+        case negativeSize:
+            printf("key of mistake: negative size of stack\n");
+            break;
+    }
+
+    printf("\n");
+
+    printf("Pointer on stack(array) = %p\n", Stack->ptrOnStack);
+    printf("Stack size = %zu\n", Stack->sizeOfStack);
+    printf("Pointer on Stack: %p\n", Stack);
+
+    printStack(Stack);
+
+   // exit(1);
+}
+
+void stackResize(struct myStack(TYPE)* Stack, size_t newSize) {
+    stackAssert(Stack, INFORMATION);
+
+    Stack->ptrOnStack = (TYPE*) realloc(Stack->ptrOnStack, newSize);
+
+    stackAssert(Stack, INFORMATION);
+}
+
+void printStack(struct myStack(TYPE)* Stack) {
+    size_t index = 0;
+
+    while(index < Stack->sizeOfStack) {
+        if(strcmp(myStackType(TYPE), "myStack_int") == 0) {
+            printf("%d ", *((Stack->ptrOnStack) + index));
+        }
+
+        if(strcmp(myStackType(TYPE), "myStack_char") == 0) {
+            printf("%c ", *((Stack->ptrOnStack) + index));
+        }
+
+        if(strcmp(myStackType(TYPE), "myStack_long long") == 0) {
+            printf("%ld ", *((Stack->ptrOnStack) + index));
+        }
+
+        ++index;
+    }
+
+    printf("\n");
+}
+
+void stackCopy(struct myStack(TYPE)* stackTo, struct myStack(TYPE)* stackFrom) {
+
+}
 
 #endif // STACK_H_INCLUDED
